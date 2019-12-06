c09f4313988dc97fa94bd558af9570b9dc94e144
jkbenaim@gmail.com <jkbenaim@gmail.com>
2019-12-06 03:57:39 +0000

    improve sa
    FossilOrigin-Name: fe9da16ee512983957be8507a7e5b6ccc9b1abaa2c240d0b0f96d109377b6ceb

--- sa.c
+++ sa.c
@@ -2,45 +2,53 @@
 #include <string.h>
 #include "sa.h"
 
-
-#include <stdio.h>
-
+size_t _sa_next(size_t old) {
+	return old + 100;
+}
 
 bool _sa_init (struct _string_array *sa)
 {
-	sa->num_strings = 0;
+	sa->slots_used = 0;
 	sa->total_len = 0;
-	sa->strings = malloc(0);
+	sa->slots_available = 0;
+	sa->strings = NULL;
 	return true;
 }
 
 void _sa_free (struct _string_array *sa)
 {
-	for (size_t i = 0; i < sa->num_strings; i++) {
+	for (size_t i = 0; i < sa->slots_used; i++) {
 		free(sa->strings[i]);
 	}
 	free(sa->strings);
+	sa->slots_used = 0;
+	sa->total_len = 0;
+	sa->slots_available = 0;
+	sa->strings = NULL;
 }
 
 bool _sa_add (struct _string_array *sa, const char *s)
 {
 	if (!s)
 		return true;
-	int rc;
-	size_t new_num_strings = sa->num_strings + 1;
-
-	char **temp = reallocarray(sa->strings, new_num_strings,
-							sizeof(char *));
-	if (temp == NULL) return false;
-
-	sa->strings = temp;
+	
+	if (sa->strings == NULL) {
+		sa->slots_available = _sa_next(0);
+		sa->slots_used = 0;
+		sa->total_len = 0;
+		sa->strings = calloc(sa->slots_available, sizeof(char *));
+	} else if (sa->slots_used == sa->slots_available) {
+		size_t new_slots_available = _sa_next(sa->slots_available);
+		char **temp = realloc(sa->strings,
+			new_slots_available * sizeof(char *));
+		if (temp == NULL) return false;
+		sa->slots_available = new_slots_available;
+		sa->strings = temp;
+	}
 
 	char *newstring = strdup(s);
-	sa->strings[new_num_strings - 1] = newstring;
-
-	size_t new_total_len = sa->total_len + strlen(newstring);
-	sa->num_strings = new_num_strings;
-	sa->total_len = new_total_len;
+	sa->strings[sa->slots_used++] = newstring;
+	sa->total_len = sa->total_len + strlen(newstring);
 	return true;
 }
 
@@ -48,8 +56,7 @@ char *_sa_get (struct _string_array *sa)
 {
 	char *t = malloc(sa->total_len + 1);
 	char *p = t;
-	printf("_sa_get: %d strings, %d bytes\n", sa->num_strings, sa->total_len+1);
-	for (size_t i = 0; i < sa->num_strings; i++) {
+	for (size_t i = 0; i < sa->slots_used; i++) {
 		size_t len = strlen(sa->strings[i]);
 		memcpy(p, sa->strings[i], len);
 		p += len;
--- sa.h
+++ sa.h
@@ -2,15 +2,23 @@
 #define _SA_H_
 #include <stdbool.h>
 
-struct _string_array {
-	size_t num_strings;
-	size_t total_len;
-	char **strings;
+struct _sa_string_t {
+	char *string;
+	void (*dtor)(char *);
 };
 
+struct _string_array {
+	size_t slots_used;
+	size_t total_len;
+	size_t slots_available;
+	char **strings;
+	//struct _sa_string_t *strings;
+};
+
+size_t _sa_next(size_t old) __attribute__((pure));
 bool _sa_init (struct _string_array *sa);
 void _sa_free (struct _string_array *sa);
-bool _sa_add (struct _string_array *sa, const char *s);
+bool _sa_add (struct _string_array *sa, const char *s) __attribute__((hot));
 char *_sa_get (struct _string_array *sa);
 
 #endif
