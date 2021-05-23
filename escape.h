2dfdec2c08558aa40393c293d15a62911bf2d509
jkbenaim@gmail.com <jkbenaim@gmail.com>
2021-05-23 10:40:55 +0000

    escape functions should not take null args
    FossilOrigin-Name: 836c197eb078935c07cbd7f90a23e9362ff6395ffcbee66c76b07b8456bea948

--- escape.h
+++ escape.h
@@ -1,6 +1,6 @@
 #ifndef _ESCAPE_H_
 #define _ESCAPE_H_
-char *escape_url(const char *orig);
-char *escape_xml(const char *orig);
+char *escape_url(const char *orig) __attribute__((nonnull(1)));
+char *escape_xml(const char *orig) __attribute__((nonnull(1)));
 #endif
 
