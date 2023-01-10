cb844b66015aef4091e6cb96e5d07bbd4dbafd69
jkbenaim@gmail.com <jkbenaim@gmail.com>
2023-01-10 03:06:45 +0000

    add script to create hashfile
    FossilOrigin-Name: 0654e64282833677c50fdf570ce99eecaaa39047f9bff39c0f28d29dbe3f7935

---
+++ mkhashfile.sql
@@ -0,0 +1,29 @@
+CREATE TEMP TABLE myhashes(
+	disc_id INT,
+	hashtype TEXT,
+	hash TEXT,
+	filename TEXT
+);
+
+INSERT INTO myhashes(
+	disc_id,
+	hashtype,
+	hash,
+	filename
+) SELECT
+	disc_id,
+	hashtype,
+	hash,
+	NULL
+FROM hashes;
+
+UPDATE myhashes SET filename=(SELECT filename FROM discs WHERE discs.disc_id=myhashes.disc_id);
+UPDATE myhashes SET filename=(SELECT name || '.iso' FROM discs WHERE discs.disc_id=myhashes.disc_id) WHERE filename IS NULL;
+
+.headers off
+.mode list
+SELECT 
+	hashtype || ' (' || filename || ') = ' || hash
+FROM myhashes ORDER BY filename;
+
+DROP TABLE temp.myhashes;
