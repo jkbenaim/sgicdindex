6e5cf1f84ceacefba71a515f273ebed2f653d331
jkbenaim <jkbenaim@gmail.com>
2024-05-05 17:40:18 +0000

    discs from CB_HK
    FossilOrigin-Name: 2888fd092df1b2e27a9ab88e0b3900039ff3ac0bb07a650fa0921b6234e60aaf

--- mkcds.c
+++ mkcds.c
@@ -154,6 +154,7 @@ int callback_sgi_cds()
 		"<h2>SGI/IRIX CDs</h2>\n"
 		"<hr/>\n"
 		"<h3>News</h3>\n"
+		"<p>2024-05-05: Added a number of new discs courtesy of CB_HK, including several Hot Mixes. Thank you!</p>\n"
 		"<p>2024-03-15: Added PDF Generator 1.2, courtesy of WildOne69. Thank you!</p>\n"
                 "<p>2023-11-26: Regenerated all tars with <a href=\"/efsextract\">efsextract</a>, and added tars for discs which did not previously have them.</p>\n"
 		"<p>2023-11-03: Added some discs from my own collection that needed redumps. Two versions each of IRIX 4.0.2 and IRIS Development Option 4.0 are now available, along with another ProPack.</p>\n"
