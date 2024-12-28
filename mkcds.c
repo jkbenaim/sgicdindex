e4b443d4f132d5a2030085c5313c61bfd63c0317
jkbenaim <jkbenaim@gmail.com>
2024-12-28 07:01:52 +0000

    new discs from joshyfishy22
    FossilOrigin-Name: 2f8c3a8dc750dcf0cf0bc11a51c6d01658aec5a874af06dec0fefd87ae7e4f45

--- mkcds.c
+++ mkcds.c
@@ -156,6 +156,7 @@ int callback_sgi_cds()
 		"<h2>SGI/IRIX CDs</h2>\n"
 		"<hr/>\n"
 		"<h3>News</h3>\n"
+		"<p>2024-12-28: Added a few support discs, courtesy of joshyfishy22. Thank you!</p>\n"
 		"<p>2024-12-04: Added a few more discs courtesy of VoxAndrews. Thank you!</p>\n"
 		"<p>2024-12-03: Added DANG Rev C Patches courtesy of VoxAndrews. Thank you!</p>\n"
 		"<p>2024-11-02: Added many more discs courtesy of chulofiasco, from the desk of bipin. Thank you both!</p>\n"
